735c14da040e3f80311eae54e14e7ad7864efbcb
smartfishman <393558792@qq.com>
2019-06-19 10:30:52 +0800

    测试提交。

---
+++ arithmetic-c++-practice/redBlackTree.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "redBlackTree.h"
+
+
+redBlackTree::redBlackTree()
+{
+}
+
+
+redBlackTree::~redBlackTree()
+{
+}
---
+++ arithmetic-c++-practice/redBlackTree.h
@@ -0,0 +1,8 @@
+#pragma once
+class redBlackTree
+{
+public:
+    redBlackTree();
+    virtual ~redBlackTree();
+};
+
